cf95e8ef1e6c50bcbf386cef247a87e8ce48dc95
Janne Peippo <peippo@gmail.com>
2022-09-01 19:32:45 +0300

    Migrate from CRA to Vite

--- src/store.jsx
+++ src/store.jsx
@@ -5,8 +5,8 @@ export const StoreContext = createContext(null);
 
 export const StoreProvider = ({ children }) => {
 	const supabase = createClient(
-		process.env.REACT_APP_SUPABASE_URL,
-		process.env.REACT_APP_SUPABASE_KEY
+		import.meta.env.VITE_SUPABASE_URL,
+		import.meta.env.VITE_SUPABASE_KEY
 	);
 
 	const fetchMessageCount = 49;
